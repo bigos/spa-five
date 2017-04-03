ab77b90149cc42ff72a1eaa532dae4067ca23396
Jacek Podkanski <ruby.object@googlemail.com>
2017-04-03 16:45:31 +0100

    added example JS code from the web

--- src/parenscripts.lisp
+++ src/parenscripts.lisp
@@ -2,10 +2,24 @@
 
 (defun parenscripts-js ()
   (ps
-   (defun greeting-callback ()
-     (alert "Hello World and everyone else"))
+    (defun greeting-callback ()
+      (alert "Hello World and everyone else"))
 
-   (defun hiding-callback ()
-     (parenscript:chain ($ "footer") (toggle)))
+    (defun hiding-callback ()
+      (chain ($ "footer") (toggle)))
 
-   ))                                   ; end of parenscript:ps
+    ;; example code from http://imgur.com/qg6mn1z
+    (progn (var your_drink)
+           (var reverse (lambda (s)
+                          (chain s (split "") (reverse) (join ""))))
+           (var bartender (create
+                           str1 "ers"
+                           str2 (reverse "rap")
+                           str3 "amet"
+                           request (lambda (preference)
+                                     (+ preference ".Secret word: "
+                                        (+ (@ this str2)
+                                           (@ this str3)
+                                           (@ this str1)))))))
+
+  ))                                    ; end of parenscript:ps
